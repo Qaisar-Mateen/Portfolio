e57395c85ebbb7e4f4cb1f72e7b0d6d8879a9c52
Qaisar-Mateen <qaisarmateen313@gmail.com>
2025-01-29 20:10:22 +0500

    bump version to 1.0.0 and add Spline dependencies

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 import Typewriter from "typewriter-effect";
+import Spline from '@splinetool/react-spline';
 
 const Hero = () => {
   return (
@@ -33,7 +34,8 @@ const Hero = () => {
         </div>
       </div>
 
-      <ComputersCanvas />
+      {/* <ComputersCanvas /> */}
+      <Avatar />
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
@@ -57,3 +59,13 @@ const Hero = () => {
 };
 
 export default Hero;
+
+function Avatar() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/rBdn0bzcfwYEE6d2/scene.splinecode" 
+      />
+    </main>
+  );
+}
