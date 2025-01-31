5d92005f71082432fb0405dab62e34263fad5413
Qaisar-Mateen <qaisarmateen313@gmail.com>
2025-01-31 22:17:40 +0500

    update resume URL in Navbar and comment out Work link in navLinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   const toggleResume = () => {
-    const resumeUrl = '/Resume.pdf';
+    const resumeUrl = '/Portfolio/Resume.pdf';
     window.open(resumeUrl);
   };
 
