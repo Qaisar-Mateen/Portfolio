3dff55f085104f82dec7dbe75f0d6ed463cb1369
Qaisar-Mateen <qaisarmateen313@gmail.com>
2025-02-01 00:19:29 +0500

    bump version to 1.1.0 and update scene URL for Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -34,9 +34,9 @@ function mouseAnimation() {
 function Particles() {
   const sceneUrl = isMobileDevice()
     ? "https://prod.spline.design/XXi-LdKqImqN5qeM/scene.splinecode"
-    : "https://prod.spline.design/rBdn0bzcfwYEE6d2/scene.splinecode";
+    : "https://prod.spline.design/k52-LDbN7C12ciir/scene.splinecode";
   return (
-    <div style={{"zIndex": 10}}>
+    <div>
       <Spline
         scene={sceneUrl}
       />
@@ -49,7 +49,7 @@ const Hero = () => {
     <>
     
     <section className="relative w-full h-screen mx-auto">
-      <div className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
+      <div  className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`} style={{zIndex: 1, pointerEvents: 'none'}}> 
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
