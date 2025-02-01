8f7034dcdceaea71f040623661ab6ea2e78242fb
Qaisar-Mateen <qaisarmateen313@gmail.com>
2025-02-01 18:19:56 +0500

    refactor Experience and Works components to update SectionWrapper parameters and modify navLinks for improved navigation

--- src/constants/index.js
+++ src/constants/index.js
@@ -27,10 +27,10 @@ export const navLinks = [
     id: "about",
     title: "About",
   },
-  // {
-  //   id: "work",
-  //   title: "Work",
-  // },
+  {
+    id: "projects",
+    title: "Projects",
+  },
   {
     id: "contact",
     title: "Contact",
