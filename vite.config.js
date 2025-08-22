a11b4fe32b5e33251f5466ee695574be400526c0
Qaisar-Mateen <qaisarmateen313@gmail.com>
2025-08-22 16:42:59 +0500

    update index.html to comment out noscript tag; adjust Vite config base path to restore previous setting

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // base: "/Portfolio/",
-  base: "/",
+  base: "/Portfolio/",
+  // base: "/",
   build: {
     rollupOptions: {
       output: {
